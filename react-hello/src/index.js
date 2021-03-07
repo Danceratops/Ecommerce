53cef50d9714d5ed44848df1966f3290930a0423
Danielle Reed <dan.reed.993@gmail.com>
2021-03-06 20:32:03 -0500

    Add props practice

--- react-hello/src/index.js
+++ react-hello/src/index.js
@@ -8,6 +8,10 @@ function HelloWorld () {
       <span>{isHello ? "hello" : "not hello"}</span>
       <Hello />
       <World />
+      <Danielle />
+      <Howdy name="danielle"/>
+      <Hola name="danielle" />
+      <Hallo fname="danielle" lname="reed"/>
     </div>
   );
 }
@@ -24,5 +28,30 @@ function World () {
     <span> World!</span>
   ); 
 }
+
+function Danielle () {
+  const firstName = "Danielle";
+  const lastName = "Reed";
+
+  return (
+    <Person age={72} name = {firstName + " " + lastName}/>
+  )
+}
+
+const Person = props => <h1>{props.name + " " + props.age}</h1>
+
+function Howdy(props) {
+  return (
+    <h1>Howdy, {props.name}</h1>
+  );
+}
+
+const Hola = ({name}) => (
+  <h1>Hola, {name}</h1>
+);
+
+const Hallo = ({fname, lname}) => (
+  <h1>Hallo, {fname} {lname}</h1>
+);
 
 ReactDom.render(<HelloWorld/>, document.querySelector('#root'));
